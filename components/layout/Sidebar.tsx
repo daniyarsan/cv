d063d75322d042f8fe21a97409a047690fe1cff6
Daniyar Akylbekov <daniyar.san@gmail.com>
2024-11-15 15:06:33 +0600

    Edit sidebar

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -67,13 +67,13 @@ export default function Sidebar() {
 
             <Link href="/cv1.pdf">
               <Button className="min-h-[46px] bg-blue-700 px-3 py-4 !text-light-900">
-                Download CV
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                   <path
                     fill="currentColor"
                     d="M13 10h5l-6 6l-6-6h5V3h2zm-9 9h16v-7h2v8a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-8h2z"
                   ></path>
                 </svg>
+                Download CV
               </Button>
             </Link>
           </div>
